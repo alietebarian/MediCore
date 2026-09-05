bb577d324996deb32bc06cf0d31b53e5014f6d04
alietebarian <alietebarianbusiness@gmail.com>
2026-09-05 15:52:09 +0330

    fix appointment dto

--- Backend/Application/Appointments/Queries/GetAppointments/AppointmentDto.cs
+++ Backend/Application/Appointments/Queries/GetAppointments/AppointmentDto.cs
@@ -3,16 +3,17 @@
 namespace Application.Appointments.Queries.GetAppointments;
 
 public record AppointmentDto
-(
-    Guid Id,
-    Guid DoctorId,
-    string DoctorName,
-    Guid PatientId,
-    string PatientName,
-    Guid ClinicId,
-    string ClinicName,
-    DateOnly Date,
-    TimeOnly StartTime,
-    TimeOnly EndTime,
-    AppointmentStatus Status,
-    string? Notes);
+{
+    public Guid Id { get; init; }
+    public Guid DoctorId { get; init; }
+    public string DoctorName { get; init; } = default!;
+    public Guid PatientId { get; init; }
+    public string PatientName { get; init; } = default!;
+    public Guid ClinicId { get; init; }
+    public string ClinicName { get; init; } = default!;
+    public DateOnly Date { get; init; }
+    public TimeOnly StartTime { get; init; }
+    public TimeOnly EndTime { get; init; }
+    public AppointmentStatus Status { get; init; }
+    public string? Notes { get; init; }
+}
--- Backend/Application/Common/Mappings/MappingProfile.cs
+++ Backend/Application/Common/Mappings/MappingProfile.cs
@@ -12,7 +12,6 @@ public class MappingProfile : Profile
     {
         CreateMap<Clinic, ClinicDto>();
         CreateMap<Specialty, SpecialistyDto>();
-        //CreateMap<Appointment, AppointmentDto>();
         CreateMap<Appointment, AppointmentDto>()
             .ForMember(x => x.DoctorName, xx => xx.MapFrom(xxx => xxx.Doctor.FullName))
             .ForMember(dest => dest.PatientName, opt => opt.MapFrom(src => src.Patient.FirstName + " " + src.Patient.LastName))
