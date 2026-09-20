3582854f1aeaee6fab16409612c608fb1047c599
alietebarian <alietebarianbusiness@gmail.com>
2026-09-20 11:56:40 +0330

    Get Medicines endpoint

---
+++ Backend/Application/Medicines/Queries/GetMedicines/GetMedicinesQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Medicines.Queries.GetMedicines;
+
+
+public record GetMedicinesQuery : IRequest<List<MedicineDto>>;
+
+public record MedicineDto(Guid Id, string Name, string? GenericName, string? Form);
---
+++ Backend/Application/Medicines/Queries/GetMedicines/GetMedicinesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Medicines.Queries.GetMedicines;
+
+public class GetMedicinesQueryHandler : IRequestHandler<GetMedicinesQuery, List<MedicineDto>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetMedicinesQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<MedicineDto>> Handle(GetMedicinesQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Medicines
+            .AsNoTracking()
+            .OrderBy(x => x.Name)
+            .Select(x => new MedicineDto(x.Id, x.Name, x.GenericName, x.Form))
+            .ToListAsync();
+    }
+}
